822b4333159c40f784cf22fb1410320b722c3872
JaesungYoun <73388615+JaesungYoun@users.noreply.github.com>
2024-01-26 15:34:19 +0900

    Title: 방화벽 설치하기, Time: 1660ms, Memory: 28MB, Status: Passed - CodeTree

--- 240126/방화벽 설치하기/firewall-installation.py
+++ 240126/방화벽 설치하기/firewall-installation.py
@@ -1,64 +1,53 @@
 from collections import deque
-N,M = map(int,input().split())
 
-
+N, M = map(int, input().split())
 
 mat = []
-visited = [[0 for _ in range(M)] for _ in range(N)]
 for _ in range(N):
-    mat.append(list(map(int,input().split())))
+    mat.append(list(map(int, input().split())))
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
 
-dx = [-1,1,0,0]
-dy = [0,0,-1,1]
 def bfs():
     global answer
     tmp_mat = [i[:] for i in mat]
-    temp = []
+
     queue = deque()
     for x in range(N):
         for y in range(M):
-            if mat[x][y] == 2:
-                queue.append((x,y))
-                temp.append((x,y))
+            if tmp_mat[x][y] == 2:
+                queue.append((x, y))
+
     while queue:
-        x,y = queue.popleft()
+        x, y = queue.popleft()
 
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
 
-            if 0<= nx < N and 0 <= ny < M and mat[nx][ny] == 0:
-                queue.append((nx,ny))
-                mat[nx][ny] = 2
+            if 0 <= nx < N and 0 <= ny < M and tmp_mat[nx][ny] == 0:
+                queue.append((nx, ny))
+                tmp_mat[nx][ny] = 2
 
     cnt = 0
     for i in range(N):
-        cnt += mat[i].count(0)
-    answer = max(answer,cnt)
-    for x in range(N):
-        for y in range(M):
-            if mat[x][y] == 2 and (x,y) not in temp:
-                mat[x][y] = 0
-
-
-
-
-
-
-
+        cnt += tmp_mat[i].count(0)
+    answer = max(answer, cnt)
 
 def backtracking(depth):
     if depth == 3:
         bfs()
         return
-
     for i in range(N):
         for j in range(M):
             if mat[i][j] == 0:
                 mat[i][j] = 1
-                backtracking(depth+1)
+                backtracking(depth + 1)
                 mat[i][j] = 0
+
+
 
 answer = 0
 backtracking(0)
