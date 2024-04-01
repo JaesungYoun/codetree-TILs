05d6c3fa30f672e756047302e120f908cd86b3ea
JaesungYoun <73388615+JaesungYoun@users.noreply.github.com>
2024-04-01 11:09:11 +0900

    Title: 팩맨, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240401/팩맨/pacman.py
+++ 240401/팩맨/pacman.py
@@ -1,117 +1,103 @@
-from collections import deque
 M,T = map(int,input().split())
-p_x,p_y = map(int,input().split())
 
-mat = [[deque() for _ in range(4)] for _ in range(4)]
+px,py = map(int,input().split())
+px -= 1
+py -= 1
+
+mat = [[[] for _ in range(4)] for _ in range(4)]
 for _ in range(M):
     x,y,d = map(int,input().split())
     mat[x-1][y-1].append(d-1)
 
-p_x -= 1
-p_y -= 1
 
-
-# 팩맨 이동 방향
+dx = [-1, -1, 0, 1, 1, 1,0,-1]
+dy = [0,-1,-1,-1,0,1,1,1]
 p_dx = [-1,0,1,0]
 p_dy = [0,-1,0,1]
-# 몬스터의 이동 방향에 대한 x, y 변화량 (상, 상우, 우, 하우, 하, 하좌, 좌, 상좌 순서)
-m_dx = [-1, -1, 0, 1, 1, 1,0,-1]
-m_dy = [0,-1,-1,-1,0,1,1,1]
 
 def monster_copy():
-    for x in range(4):
-        for y in range(4):
-            if mat[x][y]:
-                for m in range(len(mat[x][y])):
-                    temp.append((x,y,mat[x][y][m]))
-
+    for i in range(4):
+        for j in range(4):
+            if mat[i][j]:
+                for m in range(len(mat[i][j])):
+                    temp.append((i,j,mat[i][j][m]))
 def monster_move():
-    tmp = [[[] for _ in range(4)] for _ in range(4)]
 
+    tmp = [[[] for _ in range(4)] for _ in range(4)]
     for x in range(4):
         for y in range(4):
             while mat[x][y]:
                 d = mat[x][y].pop()
-                for i in range(8):
-                    nx = x + m_dx[(d+i) % 8]
-                    ny = y + m_dy[(d+i) % 8]
 
-                    if 0 <= nx < 4 and 0<= ny <4 and dead[nx][ny] == 0 and not(nx == p_x and ny == p_y):
+                for i in range(8):
+                    nx = x + dx[(d+i) % 8]
+                    ny = y + dy[(d+i) % 8]
+
+                    if 0<= nx < 4 and 0<= ny < 4 and not(px==nx and py==ny) and dead[nx][ny] == 0:
                         tmp[nx][ny].append((d+i) % 8)
                         break
-
                 else:
                     tmp[x][y].append(d)
     return tmp
 
-
-
-
-
-from collections import deque
-
-# 팩맨의 이동 방향에 대한 x, y 변화량 (상, 좌, 하, 우 순서)
-p_dx = [-1, 0, 1, 0]
-p_dy = [0, -1, 0, 1]
-
-def dfs(cnt,x,y,total,visit):
-    global max_eat,eat
-    global p_x,p_y
+def packman_move(cnt,x,y,total,visit):
+    global max_eat,px,py,eat
 
     if cnt == 3:
         if max_eat < total:
             max_eat = total
-            p_x = x
-            p_y = y
+            px = x
+            py = y
             eat = visit[:]
-
         return
 
     for i in range(4):
         nx = x + p_dx[i]
         ny = y + p_dy[i]
-        if 0<=nx<4 and 0<=ny<4:
+        if 0 <= nx < 4 and 0 <= ny < 4 :
             if (nx,ny) not in visit:
                 visit.append((nx,ny))
-                dfs(cnt+1,nx,ny,total+len(mat[nx][ny]),visit)
+                packman_move(cnt+1,nx,ny,total+len(mat[nx][ny]),visit)
                 visit.pop()
             else:
-                dfs(cnt+1,nx,ny,total,visit)
+                packman_move(cnt+1,nx,ny,total,visit)
 
+
+
+# 시체 관리 배열
 dead = [[0 for _ in range(4)] for _ in range(4)]
 
 for _ in range(T):
 
-    eat = []
-    visit = []
-    max_eat = -1
-    temp = [] # 복제한 몬스터 담을 배열
-    # 1. 몬스터 복제
+    temp = []
+    #1. 몬스터 복제
     monster_copy()
 
-# 2. 몬스터 이동
+    #2. 몬스터 이동
     mat = monster_move()
-    # 3. 팩맨 이동
-    dfs(0, p_x, p_y, 0, visit)
-    for x, y in eat:
-        if mat[x][y]:
-            mat[x][y] = []
-            dead[x][y] = 3
-    # 4. 몬스터 시체 소멸
 
+    #3. 팩맨 이동
+    visit = []
+    max_eat = -1
+    eat = []
+    packman_move(0,px,py,0,visit)
+    for x,y in eat:
+
+        mat[x][y] = []
+        dead[x][y] = 3
+    #4. 시체 소멸
     for x in range(4):
         for y in range(4):
             if dead[x][y]:
                 dead[x][y] -= 1
 
+    #.5 몬스터 복제 완성
 
-    # 5. 몬스터 복제 완성
-    for t_x,t_y,t_d in temp:
-        mat[t_x][t_y].append(t_d)
-
+    for tx,ty,td in temp:
+        mat[tx][ty].append(td)
 
 result = 0
-for x in range(4):
-    for y in range(4):
-        result += len(mat[x][y])
+for i in range(4):
+    for j in range(4):
+        result += len(mat[i][j])
 print(result)
