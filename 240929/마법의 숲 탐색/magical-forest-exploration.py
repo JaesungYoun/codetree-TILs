e56167e765b8afda6e5e1ed709d54ac1946a8b64
JaesungYoun <73388615+JaesungYoun@users.noreply.github.com>
2024-09-29 20:36:21 +0900

    Title: 마법의 숲 탐색, Time: 390ms, Memory: 29MB, Status: Passed - CodeTree

--- 240929/마법의 숲 탐색/magical-forest-exploration.py
+++ 240929/마법의 숲 탐색/magical-forest-exploration.py
@@ -6,7 +6,7 @@ mat = [[0 for _ in range(MAX_L)] for _ in range(MAX_L+3)]
 # 북 동 남 서 (-> 시계방향)
 dx = [-1,0,1,0]
 dy = [0,1,0,-1]
-isExit = [[0 for _ in range(MAX_L)] for _ in range(MAX_L)]
+isExit = [[0 for _ in range(MAX_L)] for _ in range(MAX_L+3)]
 
 def resetMat():
     for i in range(r + 3):
@@ -15,7 +15,7 @@ def resetMat():
             isExit[i][j] = 0
 
 def in_range(x,y):
-    return 3 <= x < r+2 and 0<= y < c
+    return 3 <= x < r+3 and 0<= y < c
 
 # x,y 로 이동할 수 있는지에 대해 체크하는 함수
 def canMove(x,y):
@@ -44,8 +44,8 @@ def bfs(x,y):
             if in_range(nx,ny) and not visit[nx][ny] and ((mat[nx][ny] == mat[x][y]) or (mat[nx][ny] != 0 and isExit[x][y])):
                 q.append((nx,ny))
                 visit[nx][ny] = True
-                result = max(result,max_x)
-    return result
+                max_x = max(max_x,nx)
+    return max_x
 
 # 아래로 이동 (골렘 출구 방향 d 변화)
 def down(x,y,d,id):
@@ -65,8 +65,6 @@ def down(x,y,d,id):
             resetMat()
         else:
             mat[x][y] = id
-            res = bfs(x,y)
-
             for i in range(4):
                 mat[x+dx[i]][y+dy[i]] = id
 
@@ -81,7 +79,7 @@ def down(x,y,d,id):
 
 
 ans = 0
-for _ in range(1,k+1):
+for id in range(1,k+1):
     ci, d = map(int,input().split())
     down(0,ci-1,d,id)
-    print(ans)
+print(ans)
