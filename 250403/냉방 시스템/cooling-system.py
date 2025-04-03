3917f7cd774cb01ace69dd40df2af4d6f77600f7
JaesungYoun <73388615+JaesungYoun@users.noreply.github.com>
2025-04-03 19:01:17 +0900

    Title: 냉방 시스템, Time: 132ms, Memory: 21MB, Status: Wrong Answer - Codetree

--- 250403/냉방 시스템/cooling-system.py
+++ 250403/냉방 시스템/cooling-system.py
@@ -1,212 +1,161 @@
-from collections import deque
-N,M,K = map(int,input().split())
+from collections import deque, defaultdict
 
-room = [] # 사무실
-aircon = [[] for _ in range(4)] # 에어컨
-result = [[0 for _ in range(N)] for _ in range(N)]
-for i in range(N):
+n,m,k = map(int,input().split())
+
+
+rooms = []
+aircons = []
+for i in range(n):
     mat = list(map(int,input().split()))
-    for j in range(N):
+    for j in range(n):
         if mat[j] == 1:
-            room.append((i,j))
+            rooms.append((i,j))
         elif mat[j] >= 2:
-            aircon[mat[j] - 2].append((i,j))
+            aircons.append((i,j,mat[j]-2))
 
 
-# 상하좌우
-wall = [[[0 for _ in range(4)] for _ in range(N)] for _ in range(N)]
-for _ in range(M):
+wall = [[[0 for _ in range(4)] for _ in range(n)] for _ in range(n)]
+
+for _ in range(m):
     x,y,s = map(int,input().split())
-    x -= 1
-    y -= 1
-
+    x-=1
+    y-=1
     if s == 1:
-        wall[x][y-1][3] = 1 # 왼쪽칸의 오른쪽 벽
-        wall[x][y][2] = 1 # 현재 칸의 왼쪽벽
+        wall[x][y-1][3] = 1
+        wall[x][y][2] = 1
     else:
-        wall[x-1][y][1] = 1 # 위쪽칸의 아래벽
-        wall[x][y][0] = 1 # 현재칸의 위쪽벽
+        wall[x-1][y][1] = 1
+        wall[x][y][0] = 1
+
+# 좌상우하
+dx = [0,-1,0,1]
+dy = [-1,0,1,0]
+coolness = [[0 for _ in range(n)] for _ in range(n)]
+
+def bfs(x,y,d,power):
+    temp = [[0 for _ in range(n)] for _ in range(n)]
+    direction_dict = defaultdict(list)
+    # 방향에 따른
+    direction_dict[0] = [(1,-1),(0,-1),(-1,-1)]
+    direction_dict[1] = [(-1,-1),(-1,0),(-1,1)]
+    direction_dict[2] = [(1,1),(0,1),(-1,1)]
+    direction_dict[3] = [(1,-1),(1,0),(1,1)]
 
 
-cool = [[0 for _ in range(N)] for _ in range(N)]
 
-def in_range(x,y):
-    if 0<= x < N and 0 <= y < N:
-        return True
-    return False
+    spread_dir_list = direction_dict[d]
+    queue = deque()
+    queue.append((x,y,power))
+    temp[x][y] = power
+    coolness[x][y] += power
+    while queue:
+        x,y,power = queue.popleft()
 
-def cooling(x,y,d):
-    temp = [[0 for _ in range(N)] for _ in range(N)]
+        for s_dx,s_dy in spread_dir_list:
+            nx = x + s_dx
+            ny = y + s_dy
+            if 0<=nx<n and 0<=ny<n and temp[nx][ny] == 0 and power > 1:
+                if d == 0 or d ==2:
+                    if s_dx != 0 and s_dy != 0:
+                        if s_dx > 0:
+                            if (wall[x][y][(d+3)%4] == 0) and wall[nx][ny][(d+2)%4] == 0:
+                                queue.append((nx,ny,power-1))
+                                temp[nx][ny] = power-1
+                                coolness[nx][ny] += power-1
+                        else:
+                            if wall[x][y][(d+1)%4] == 0 and wall[nx][ny][(d+2)%4] == 0:
+                                queue.append((nx, ny, power - 1))
+                                temp[nx][ny] = power - 1
+                                coolness[nx][ny] += power - 1
+                    else:
+                        if wall[nx][ny][(d+2)%4] == 0:
+                            queue.append((nx,ny,power-1))
+                            temp[nx][ny] = power-1
+                            coolness[nx][ny] += power-1
+                else:
+                    if s_dx != 0 and s_dy != 0:
+                        if s_dy > 0:
+                            if (wall[x][y][(d+3)%4] == 0) and wall[nx][ny][(d+2)%4] == 0:
+                                queue.append((nx,ny,power-1))
+                                temp[nx][ny] = power-1
+                                coolness[nx][ny] += power-1
+                        else:
+                            if wall[x][y][(d+1)%4] == 0 and wall[nx][ny][(d+2)%4] == 0:
+                                queue.append((nx, ny, power - 1))
+                                temp[nx][ny] = power - 1
+                                coolness[nx][ny] += power - 1
+                    else:
+                        if wall[nx][ny][(d + 2) % 4] == 0:
+                            queue.append((nx, ny, power-1))
+                            temp[nx][ny] = power-1
+                            coolness[nx][ny] += power-1
+def spread(ax,ay,d):
 
-    start = 5 # 초기값 5
+    power = 5
+    nx = ax + dx[d]
+    ny = ay + dy[d]
 
-
-    if d == 0: # 에어컨 방향이 왼쪽
-        queue = deque()
-        
-        temp[x][y-1] = start
-        queue.append((x,y-1,start))
-        while queue:
-            x,y,cool = queue.popleft()
-
-            if in_range(x,y-1) and wall[x][y-1][3] == 0:
-                temp[x][y-1] = cool - 1
-                if cool -1 > 1:
-                    queue.append((x,y-1,cool-1))
-            if in_range(x-1,y-1) and wall[x-1][y][1] == 0 and wall[x-1][y-1][3] == 0:
-                temp[x-1][y-1] = cool - 1
-                if cool - 1 > 1:
-                    queue.append((x-1,y-1,cool-1))
-            if in_range(x+1,y-1) and wall[x+1][y][0] == 0 and wall[x+1][y-1][3] == 0:
-                temp[x+1][y-1] = cool - 1
-                if cool - 1 > 1:
-                    queue.append((x+1,y-1,cool-1))
-
-        for i in range(N):
-            for j in range(N):
-                result[i][j] += temp[i][j]
-
-
-    elif d == 1: # 북쪽 방향
-        queue = deque()
-        
-        temp[x-1][y] = start
-        queue.append((x-1,y,start))
-
-        while queue:
-            x,y,cool = queue.popleft()
-
-            if in_range(x-1,y) and wall[x-1][y][1] == 0:
-                temp[x-1][y] = cool - 1
-                if cool - 1 > 1:
-                    queue.append((x-1,y,cool-1))
-            if in_range(x-1,y-1) and wall[x][y-1][3] == 0 and wall[x-1][y-1][1] == 0:
-                temp[x-1][y-1] = cool -1
-                if cool - 1 > 1:
-                    queue.append((x-1,y-1,cool-1))
-            if in_range(x-1,y+1) and wall[x][y+1][2] == 0 and wall[x-1][y+1][1] == 0:
-                temp[x-1][y+1] = cool -1
-                if cool - 1 > 1:
-                    queue.append((x-1,y+1,cool-1))
-        for i in range(N):
-            for j in range(N):
-                result[i][j] += temp[i][j]
-
-    elif d == 2: # 에어컨 방향이 오른쪽
-        queue = deque()
-        
-        temp[x][y+1] = start
-        queue.append((x,y+1,start))
-        while queue:
-            x,y,cool = queue.popleft()
-            if in_range(x,y+1) and wall[x][y+1][2] == 0:
-                temp[x][y+1] = cool - 1
-                if cool - 1 > 1:
-                    queue.append((x,y+1,cool-1))
-            if in_range(x-1,y+1) and wall[x-1][y][1] == 0 and wall[x-1][y+1][2] == 0:
-                temp[x-1][y+1] = cool - 1
-                if cool - 1 > 1:
-                    queue.append((x-1,y+1,cool-1))
-            if in_range(x+1,y+1) and wall[x+1][y][0] == 0 and wall[x+1][y+1][2] == 0:
-                temp[x+1][y+1] = cool -1
-                if cool - 1 > 1:
-                    queue.append((x+1,y+1,cool-1))
-        for i in range(N):
-            for j in range(N):
-                result[i][j] += temp[i][j]
-
-    else: # 에어컨 방향이 남쪽
-        queue = deque()
-        
-        temp[x+1][y] = start
-        queue.append((x+1,y,start))
-
-        while queue:
-            x,y,cool = queue.popleft()
-
-            if in_range(x+1,y) and wall[x+1][y][0] == 0:
-                temp[x+1][y] = cool - 1
-                if cool - 1 > 1:
-                    queue.append((x+1,y,cool-1))
-            if in_range(x+1,y-1) and wall[x][y-1][3] == 0 and wall[x+1][y-1][0] == 0:
-                temp[x+1][y-1] = cool - 1
-                if cool - 1 > 1:
-                    queue.append((x+1,y-1,cool-1))
-            if in_range(x+1,y+1) and wall[x][y+1][2] == 0 and wall[x+1][y+1][0] == 0:
-                temp[x+1][y+1] = cool - 1
-                if cool - 1 > 1:
-                    queue.append((x+1,y+1,cool-1))
-
-        for i in range(N):
-            for j in range(N):
-                result[i][j] += temp[i][j]
+    bfs(nx,ny,d,power)
 
 
-dx = [-1,1,0,0]
-dy = [0,0,-1,1]
 def mixing():
     difference = []
-    for x in range(N):
-        for y in range(N):
+    for x in range(n):
+        for y in range(n):
             for d in range(4):
                 nx = x + dx[d]
                 ny = y + dy[d]
-                if not in_range(nx,ny) or result[x][y] <= result[nx][ny]:
+                if not (0<=nx<n and 0<=ny<n) or coolness[x][y] <= coolness[nx][ny]:
                     continue
                 if wall[x][y][d] == 1:
                     continue
-                diff = (result[x][y] - result[nx][ny]) // 4
+                diff = (coolness[x][y] - coolness[nx][ny]) // 4
                 if diff > 0:
                     difference.append((x,y,-diff))
                     difference.append((nx,ny,diff))
     for i,j,diff in difference:
-        result[i][j] += diff
-
+        coolness[i][j] += diff
 
 
 def reduce():
 
-    for i in range(N-1):
-        result[0][i] -= 1
-    for i in range(1,N):
-        result[N-1][i] -= 1
+    for i in range(n-1):
+        coolness[0][i] -= 1
+    for i in range(1,n):
+        coolness[n-1][i] -= 1
 
-    for i in range(1,N):
-        result[i][0] -= 1
+    for i in range(1,n):
+        coolness[i][0] -= 1
 
-    for i in range(N-1):
-        result[i][N-1] -= 1
+    for i in range(n-1):
+        coolness[i][n-1] -= 1
 
-    for i in range(N):
-        for j in range(N):
-            result[i][j] = max(0, result[i][j])
+    for i in range(n):
+        for j in range(n):
+            coolness[i][j] = max(0, coolness[i][j])
 
 time = 0
-
 while 1:
     flag = True
     time += 1
+
     if time > 100:
         print(-1)
-        exit(0)
+        break
 
     # 1. 시원한 공기 이동
-    for i in range(4):
-        if aircon[i]:
-            for ax, ay in aircon[i]:
-                cooling(ax,ay,i)
+    for ax,ay,d in aircons:
+        spread(ax,ay,d)
 
     # 2. 시원한 공기들이 섞임
     mixing()
 
     # 3. 외벽에 있는 칸에 대해서만 시원함이 1씩 감소
     reduce()
-    for rx, ry in room:
-        if result[rx][ry] < K:
+    for rx, ry in rooms:
+        if coolness[rx][ry] < k:
             flag = False
             break
     if flag:
         print(time)
         break
-
-
